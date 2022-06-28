f26f92511013956a17e9f357e7c637bd1c0b79c5
Paul Spencer <pwablito@gmail.com>
2022-06-28 18:14:46 -0400

    basic problem and solver classes

---
+++ combinational_sum/solver/problem.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class CombinationalSumProblem:
+    def __init__(self, target: float, available_values: List[float]):
+        self.target = target
+        self.available_values = available_values
---
+++ combinational_sum/solver/solver.py
@@ -0,0 +1,6 @@
+from combinational_sum.solver.problem import CombinationalSumProblem
+
+
+class CombinationalSumSolver:
+    def __init__(self, problem: CombinationalSumProblem):
+        self.problem = problem
