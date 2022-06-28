27a07f0a65da791d29c5c56da78fb1e5ed261ef5
Paul Spencer <pwablito@gmail.com>
2022-06-28 18:27:18 -0400

    Interface with solver backend

--- combinational_sum/gui/window.py
+++ combinational_sum/gui/window.py
@@ -1,8 +1,13 @@
+import json
 import math
 
 from PyQt5.QtWidgets import QLabel, QLineEdit, QMainWindow, QPushButton
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QDoubleValidator
+
+from combinational_sum.solver.problem import CombinationalSumProblem
+from combinational_sum.solver.solver import CombinationalSumSolver
+
 
 WINDOW_DIMENSIONS = (400, 400)
 WINDOW_START_LOCATION = (50, 50)
@@ -41,10 +46,21 @@ class MainWindow(QMainWindow):
         self.output_text.move(20, 140)
 
     def on_click(self):
-        new_text = f"Target value: {self.target_value_line.text()}"
-        new_text += f"\nAvailable values: {self.available_values_line.text()}"
-        self.output_text.setText(new_text)
-        # TODO make it actually do the calculations and put the result in self.output_text
+        self.output_text.setText(
+            f"Target value: {self.target_value_line.text()}\nAvailable values: {self.available_values_line.text()}"
+        )
+        try:
+            problem = CombinationalSumProblem.from_strings(
+                self.target_value_line.text(), self.available_values_line.text(),
+            )
+            self.output_text.setText("Solving...")
+            solver = CombinationalSumSolver(problem)
+            solution = solver.solve()
+            self.output_text.setText(json.dumps(solution))
+        except NotImplementedError:
+            self.output_text.setText("Something wasn't implemented...")
+        except ValueError:
+            self.output_text.setText("Failed to parse the input...")
 
     def run(self):
         self.widget.show()
