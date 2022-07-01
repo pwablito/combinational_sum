8f6507a32e86a97594d03615be7be3ca2c1e3845
Paul Spencer <pwablito@gmail.com>
2022-07-01 16:49:17 -0400

    type hint for solve

--- combinational_sum/solver/solver.py
+++ combinational_sum/solver/solver.py
@@ -1,9 +1,10 @@
 from combinational_sum.solver.problem import CombinationalSumProblem
+from combinational_sum.solver.solution import CombinationalSumSolution
 
 
 class CombinationalSumSolver:
     def __init__(self, problem: CombinationalSumProblem):
         self.problem = problem
 
-    def solve(self):
+    def solve(self) -> CombinationalSumSolution:
         raise NotImplementedError
