8e2e77102d5c95ca6a9ebf276d7dddb553c6926d
Spencer, Paul Whiting <pwablito@gmail.com>
2022-07-30 16:56:38 -0400

    add no-gui flag

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from combinational_sum.gui.app import App
 
+import argparse
 import sys
 
 
 def main():
-    sys.exit(App().run())
+    parser = argparse.ArgumentParser("Combinational Sum Solver")
+    parser.add_argument("--no-gui", action="store_true")
+    args = parser.parse_args()
+    if args.no_gui:
+        # TODO implement the non-gui version of the application
+        pass
+    else:
+        sys.exit(App().run())
 
 
 if __name__ == "__main__":
